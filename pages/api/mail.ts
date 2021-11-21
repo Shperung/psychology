b9463d024680576700dcd0461c0a0d5976576d5a
Viktor Kravchuk <viktorkrav4uk87@gmail.com>
2021-11-21 22:02:22 +0200

    add mail

---
+++ pages/api/mail.ts
@@ -0,0 +1,73 @@
+import nodemailer from 'nodemailer';
+
+// Step 1
+let transporter = nodemailer.createTransport({
+  service: 'gmail',
+  auth: {
+    user: process.env.EMAIL,
+    pass: process.env.PASSWORD,
+  }
+});
+
+const globalMailOptions = {
+  from: 't.g.redko@gmail.com',
+  to: 'viktorkrav4uk87@gmail.com, t.g.redko@gmail.com',
+};
+
+
+const jsonHeaders = {
+  Accept: 'application/json',
+  "Content-Type": "application/json"
+}
+
+
+export default function mail(req, res) {
+
+  console.log('process.env.EMAIL', process.env.EMAIL);
+  console.log('process.env.PASSWORD', process.env.PASSWORD);
+  console.log('process.env', process.env);
+
+  if (req.body.name && req.body.phone) {
+    const mailOptions = {   
+      ...globalMailOptions,
+       subject: 'Повідомлення з сайту tetiana-redko.com',
+       text: `Відправлена заявка з сайту 
+         \n Ім’я - ${req.body.name}
+         \n Телефон - ${req.body.phone}
+         \n Eail - ${req.body.email || ''}
+         \n Повідомлення - ${req.body.text || ''}
+         \n --------------------------
+       `,
+    }
+
+    transporter.sendMail(mailOptions, function (err, data) {
+     if (err) {
+       console.log('err', err);
+       console.log('Не вдалось відправити заявку. Спробуйте пізніше');
+       const msgs = JSON.stringify({
+        msg: "Не вдалось відправити заявку. Спробуйте пізніше",
+        status: 'error',
+      });
+      res.writeHead(400, jsonHeaders);
+      res.end(msgs);
+     } else {
+        console.log('Заявка успішно відправлена');
+        const msgs = JSON.stringify({
+          msg: "Заявка успішно відправлена",
+          status: 'success',
+        });
+        res.writeHead(200, jsonHeaders);
+        res.end(msgs);
+     }
+    });
+    } else {
+    const msgs = JSON.stringify({
+      msg: "Перевірте введені дані",
+      status: 'info',
+    });
+    res.writeHead(400, jsonHeaders);
+    res.end(msgs);
+  }
+
+
+}
