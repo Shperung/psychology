5984999bac5bb839566c8c7be18193da5c0f63e7
Viktor Kravchuk <viktorkrav4uk87@gmail.com>
2021-12-01 22:23:29 +0200

    fix crossorigin

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -11,7 +11,7 @@ class MainDocument extends Document {
       <Html lang="ua">
         <Head>
           <link rel="preconnect" href="https://fonts.googleapis.com" />
-          <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin />
+          <link rel="preconnect" href="https://fonts.gstatic.com" />
           <link
             href="https://fonts.googleapis.com/css2?family=Comfortaa:wght@400;500;700&display=swap"
             rel="stylesheet"
