3acf64d806c1a5e3cd16b721c2eddc49ff27feb1
Viktor Kravchuk <vkravchuk.externe@bedrockstreaming.com>
2024-09-28 22:44:06 -0700

    update messagers

--- components/contacts/phones.tsx
+++ components/contacts/phones.tsx
@@ -16,19 +16,19 @@ export default function PhonesBlock() {
         <a href="tel:+380939167544">+38-093-916-75-44</a>
       </span>
       <span>
-        <a href="viber://chat?number=%2B380939167544">
+        <a style={{display: "flex"}} href="viber://chat?number=%2B380939167544">
           <img src="/icons/viber.svg" alt="viber" width="24" height="24" />
           (viber)
         </a>
       </span>
       <span>
-        <a href="https://t.me/+380939167544">
+        <a style={{display: "flex"}} href="https://t.me/+380939167544">
           <img src="/icons/telegram.svg" alt="telegram" width="24" height="24" />
           (telegram)
         </a>
       </span>
       <span>
-        <a href="https://wa.me/+380939167544">
+        <a style={{display: "flex"}} href="https://wa.me/+380939167544">
           <img src="/icons/whatsapp.svg" alt="whatsapp" width="24" height="24" />
           (whatsapp)
         </a>
